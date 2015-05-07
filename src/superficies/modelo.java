878107172d6f9942c225cc630cbcd01d16d0bca5
mcividanesbaz <mcividanesbaz@atanasoft19.danielcastelao.org>
2015-05-07 09:39:34 +0200

    clase modelo metodos en proceso

--- src/superficies/modelo.java
+++ src/superficies/modelo.java
@@ -1,67 +1,72 @@
 
 package superficies;
 
+import java.util.Scanner;
+
 
 public class modelo {
    
    public modelo(){}
     
     
+    private static Scanner sc; 
     
     
-    
-    public void calcular(){
-        
-        
+    public void calcularCuadrado(cuadrado cua){
+       
         if (shapeType.equals("square")) {
             //add area calculations for square
-            float squareArea;
-            float sideLength;
+           
 
             System.out.println("what is the side length?");
-            sideLength = sc.nextFloat();
-            squareArea = sideLength * sideLength;
-            System.out.println("The area for your square: " + squareArea);
+            cua.sideLength = sc.nextFloat();
+            cua.squareArea = cua.sideLength * cua.sideLength;
+            System.out.println("The area for your square: " + cua.squareArea);
 
         }
+    }
+    
+    
+        public void calcularRectangulo(rectangulo rec){
         if (shapeType.equals("rectangle")) {
             //add area calculations for rectangle here
-            float sideLength;
-            float sideHeight;
-            float rectangleArea;
+        
 
             System.out.println("what is the rectangles width?");
             sideLength = sc.nextFloat();
             System.out.println("What is the rectangles height?");
             sideHeight = sc.nextFloat();
-            rectangleArea = sideLength * sideHeight;
-            System.out.println("The area for your rectangle is: " + rectangleArea);
+            
+            rec.rectangleArea = rec.sideLength * rec.sideHeight;
+            System.out.println("The area for your rectangle is: " + rec.rectangleArea);
 
         }
+        }
+        
+        public void calcularTriangulo(triangulo tri){
         if (shapeType.equals("triangle")) {
             //add area calculations for triangle here
-            float baseLength;
-            float height;
-            float triangleArea;
+            
 
             System.out.println("What is the base length of the triangle?");
             baseLength = sc.nextFloat();
             System.out.println("What is the height of the triangle?");
             height = sc.nextFloat();
-            triangleArea = (float) (0.5 * baseLength * height);
-            System.out.println("Your triangles area is: " + triangleArea);
+            tri.triangleArea = (float) (0.5 * tri.baseLength * tri.height);
+            System.out.println("Your triangles area is: " + tri.triangleArea);
 
         }
+        }
+        public void calcularCirculo(circulo cir){
         if (shapeType.equals("circle")) {
             //add area calculations for a circle here
-            float radius;
-            float circleArea;
+           
 
             System.out.println("What is the radius of the circle?");
             radius = sc.nextFloat();
-            circleArea = radius * radius;
-            circleArea = (float) (3.14159265 * circleArea);
-            System.out.println("Your Circles area is " + circleArea);
+            cir.circleArea = cir.radius * cir.radius;
+            cir.circleArea = (float) (3.14159265 * cir.circleArea);
+            System.out.println("Your Circles area is " + cir.circleArea);
 
         }
 
