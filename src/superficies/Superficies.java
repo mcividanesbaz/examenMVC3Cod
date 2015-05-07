4ce45d6185f034175034dda1ce2168d9d30107ae
mcividanesbaz <mcividanesbaz@atanasoft19.danielcastelao.org>
2015-05-07 09:39:50 +0200

    main- en proceso

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -10,7 +10,9 @@ public class Superficies {
   
     public static void main(String[] args) {
         
-
+   System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
+        shapeType = sc.nextLine();
+        System.out.println("You said: " + shapeType);
       
 }
 }
