1cc4fd1ed2b68b476a48d635c385fd69cb4795dd
mcividanesbaz <mcividanesbaz@atanasoft19.danielcastelao.org>
2015-05-07 09:33:29 +0200

    clase circulo con los atributos y los geters y seters

---
+++ src/superficies/triangulo.java
@@ -0,0 +1,38 @@
+
+
+package superficies;
+
+
+public class triangulo {
+            float baseLength;
+            float height;
+            float triangleArea;
+    
+    public triangulo(){}
+
+    public float getBaseLength() {
+        return baseLength;
+    }
+
+    public void setBaseLength(float baseLength) {
+        this.baseLength = baseLength;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getTriangleArea() {
+        return triangleArea;
+    }
+
+    public void setTriangleArea(float triangleArea) {
+        this.triangleArea = triangleArea;
+    }
+    
+    
+}
