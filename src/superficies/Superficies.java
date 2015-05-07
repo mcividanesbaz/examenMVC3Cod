5cdb575a70736ecbd6dd9049c63763e9c463fad3
mcividanesbaz <mcividanesbaz@atanasoft19.danielcastelao.org>
2015-05-07 10:12:37 +0200

    commit final con todos los cambios que tuve que hacer a ultima hora

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -5,14 +5,33 @@ import java.util.Scanner;
 
 public class Superficies {
 
-   
-
+    private static Scanner sc; 
+    
   
     public static void main(String[] args) {
+          vista prog = new vista();
+        prog.menu();
         
-   System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
-        shapeType = sc.nextLine();
-        System.out.println("You said: " + shapeType);
-      
+  
 }
+       public static void calcularCuadrado(cuadrado cua){
+        cua.setSquareArea(cua.getSideLength()*cua.getSideLength());
+    }
+    
+      
+    public static void calcularRectangulo(rectangulo rec){
+        rec.setRectangleArea(rec.getSideLength()*rec.getSideHeight());
+        
+    }
+    
+    public static void calcularTriangulo(triangulo tri){      
+        tri.setTriangleArea((float) (0.5 * tri.getBaseLength()* tri.getHeight()));
+        
+    }
+    
+    public static void calcularCirculo(circulo cir){
+        cir.setCircleArea((float) (3.14159265 *(cir.getRadius()*cir.getRadius())));
+        
+    }
+    
 }
--- src/superficies/vista.java
+++ src/superficies/vista.java
@@ -7,41 +7,119 @@ import java.util.Scanner;
 
 public class vista {
   
-     private static Scanner sc; 
-     
-    public void introducirCuadrado(cuadrado cua){
-    
-           String shapeType;
+     private static Scanner sc;
 
-            System.out.println("what is the side length?");
-            cua.sideLength = sc.nextFloat();
-               sc = new Scanner(System.in);   
-    }
-    
-    
-    public void introducirCirculo(circulo cir){
-       
-        
-        
-          System.out.println("What is the radius of the circle?");
-            cir.radius = sc.nextFloat();
-            sc = new Scanner(System.in);
-     
+    /**
+     * Menu con la figura que vamos a crear.
+     */
+    public void menu() {
+        String shapeType;
 
+        sc = new Scanner(System.in);
+
+        System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
+        shapeType = sc.nextLine();
+        System.out.println("You said: " + shapeType);
+
+        if (shapeType.equals("square")) {
+            cuadrado cuadrado1 = new cuadrado();
+            introducirDatosSquare(cuadrado1);
+            Superficies.calcularCuadrado(cuadrado1);
+            this.visualizarSquare(cuadrado1);
+
+        }
+        if (shapeType.equals("rectangle")) {
+            rectangulo rectangulo1 = new rectangulo();
+            introducirDatosRectangle(rectangulo1);
+            Superficies.calcularRectangulo(rectangulo1);
+            this.visualizarRectangle(rectangulo1);
+        }
+        if (shapeType.equals("triangle")) {
+            triangulo triangulo1 = new triangulo();
+            introducirDatosTriangle(triangulo1);
+            Superficies.calcularTriangulo(triangulo1);
+            this.visualizarTriangle(triangulo1);
+        }
+        if (shapeType.equals("circle")) {
+            circulo circle1 = new circulo();
+            introducirDatosCircle(circle1);
+            Superficies.calcularCirculo(circle1);
+            this.visualizarCircle(circle1);
+        }
     }
-    public void introducirRectangulo(rectangulo rec){
-        
+
+    /**
+     * Introducimos los datos de un cuadrado.
+     *
+     */
+    public void introducirDatosSquare(cuadrado cua) {
+        System.out.println("what is the side length?");
+        cua.setSideLength(sc.nextFloat());
+    }
+
+    /**
+     * Introducimos los datos de un rectangulo.
+     *
+     */
+    public void introducirDatosRectangle(rectangulo rec) {
         System.out.println("what is the rectangles width?");
-            rec.sideLength = sc.nextFloat();
-            System.out.println("What is the rectangles height?");
-            rec.sideHeight = sc.nextFloat();
-        
-        
+        rec.setSideLength(sc.nextFloat());
+        System.out.println("What is the rectangles height?");
+        rec.setSideHeight(sc.nextFloat());
     }
-    
-    
-    
-    
+
+    /**
+     * Introducimos los datos de un triangulo.
+     *
+     */
+    public void introducirDatosTriangle(triangulo tri) {
+        System.out.println("What is the base length of the triangle?");
+        tri.setBaseLength(sc.nextFloat());
+        System.out.println("What is the height of the triangle?");
+        tri.setHeight(sc.nextFloat());
+    }
+
+    /**
+     * Introducimos los datos de un circulo
+     *
+     * @param circle le pasamos el objeto tipo Circle
+     */
+    public void introducirDatosCircle(circulo cir) {
+        System.out.println("What is the radius of the circle?");
+        cir.setRadius(sc.nextFloat());
+    }
+
+    /**
+     * Visualizamos el area del rectangulo
+     *
+     */
+    public void visualizarRectangle(rectangulo rec) {
+        System.out.println("The area for your rectangle: " + rec.getRectangleArea());
+    }
+
+    /**
+     * Visualizamos el area del cuadrado
+     *
+     */
+    public void visualizarSquare(cuadrado cua) {
+        System.out.println("The area for your square: " + cua.getSquareArea());
+    }
+
+    /**
+     * Visualizamos el area del triangulo
+     *
+     */
+    public void visualizarTriangle(triangulo tri) {
+        System.out.println("Your triangles area is: " + tri.getTriangleArea());
+    }
+
+    /**
+     * Visualizamos el area del Circulo
+     *
+     */
+    public void visualizarCircle(circulo cir) {
+        System.out.println("Your Circles area is " + cir.getCircleArea());
+    }
     
     
 }
